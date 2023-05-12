72989ba4c57ba27f6a27f4777a7fe02eded29710
Muhammad Hassan Abbas <hassanabbas4745@gmail.com>
2023-05-12 10:23:07 +0500

    Add session key in env example file

--- middleware/session.js
+++ middleware/session.js
@@ -5,9 +5,12 @@
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const db = require("../db/conn");
+require('dotenv').config();
+
+const sessionKey = process.env.SESSION_KEY;
 
 const sessionMiddleware = session({
-    secret: 'your secret key',
+    secret: sessionKey,
     resave: false,
     saveUninitialized: true,
     store: new MongoStore({ mongoUrl: db.client.s.url })
