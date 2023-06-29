60068e69975323bf358ffb77a053ca393acf52a2
Hassan <hassanabbas4745@gmail.com>
2023-06-29 23:41:31 +0500

    Implement user CRUD controller functions

--- app/http/controllers/userController.js
+++ app/http/controllers/userController.js
@@ -1,6 +1,15 @@
 const User = require('../../models/user');
 
-exports.create = function(req, res, next){
+exports.index = async function(req, res, next){
+  try{
+    const users = await User.find({});
+    return res.json(users);
+  }catch(err){
+    return res.status(500).json({error: 'Internal Server Error'})
+  }
+};
+
+exports.store = function(req, res, next){
     User.register(
         new User({ 
           name: req.body.name,
@@ -8,10 +17,62 @@ exports.create = function(req, res, next){
           username: req.body.username 
         }), req.body.password, function (err, msg) {
           if (err) {
-            res.json(err);
+            return res.json(err);
           } else {
-            res.json({ message: "Successful" });
+            return res.json({ message: "Successful" });
           }
         }
       )
+};
+
+exports.show = async function(req, res, next){
+  try{
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+
+    /******************************
+     * if no user exists with
+     * given id 
+     ******************************/
+    if(!user){
+      return res.status(404).json({error: 'User not found'});
+    }
+
+    return res.json(user);
+  }catch{
+    return res.status(500).json({error: 'Internal Server Error'});
+  }
+};
+
+exports.update = async function(req, res, next){
+  try{
+    const userId = req.params.id;
+    const updatedUserData = req.body;
+
+    const user = await User.findByIdAndUpdate(userId, updatedUserData, {new: true});
+
+    if(!user){
+      return res.status(404).json({error: 'User not found'});
+    }
+
+    return res.json(user);
+  }catch{
+    return res.status(500).json({error: 'Internal Server Error'});
+  }
+};
+
+exports.destroy = async function(req, res, next){
+  try{
+    const userId = req.params.id;
+    
+    const user = await User.findByIdAndDelete(userId);
+
+    if(!user){
+      return res.status(404).json({error: 'User not found'});
+    }
+
+    return res.json({message: 'User deleted successfully'});
+  }catch{
+    return res.status(500).json({error: 'Internal Server Error'});
+  }
 };
