c55bdbc3e5409599e0ab73e4a11c17ace1353292
Hassan <hassanabbas4745@gmail.com>
2023-06-29 23:41:54 +0500

    Add user CRUD route handlers

--- routes/user.js
+++ routes/user.js
@@ -7,8 +7,11 @@ const userController = require('../app/http/controllers/userController.js');
 const router = express.Router();
 
 
-
-router.post('/create-user', userController.create)
+router.get('/users', userController.index)              //List all users
+router.post('/users', userController.store)             //Create(add) a user
+router.get('/users/:id', userController.show)           //Show a user
+router.put('/users/:id', userController.update)         //Update(edit) a user
+router.delete('/users/:id', userController.destroy)     //Delete a user
 
 
 
